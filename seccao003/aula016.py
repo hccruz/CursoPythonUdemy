5a4775c966d07eb82de3b9fa568c699986908299
Heraldo Candido da Cruz <hccruz@gmail.com>
2023-11-07 15:56:07 -0300

    Adicionando a aula 016 e arquivo debuger

---
+++ seccao003/aula016.py
@@ -0,0 +1,24 @@
+'''entrada = input('Você quer entrar ou sair? ')
+
+if entrada == 'entrar':
+    print('Você entrou no sistema.')
+elif entrada == 'sair':
+    print('Você saiu do sistema.')
+else:
+    print('Você não digitou nem entrar e nem sair.')'''
+
+condicao1 = False
+condicao2 = False
+condicao3 = True
+condicao4 = True
+
+if condicao1:
+    print('Código condicão 1')
+elif condicao2:
+    print('Código condicão 2')
+elif condicao3:
+    print('Código condicão 3')
+elif condicao4:
+    print('Código condicão 4')
+else:
+    print('Nenhuma condição executada')
