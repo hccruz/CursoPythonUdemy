efb714bda74c3f0c0b6b5ca620693d4319ac8374
Heraldo Cruz <hccruz@gmail.com>
2024-04-08 13:45:37 -0300

    Atualizacao dos exercícios

---
+++ aula156/varias_linhas.py
@@ -0,0 +1,19 @@
+"""O que seu módulo faz
+
+Lorem ipsum dolor sit amet. Qui deserunt dolores eum illum galisum in
+consequatur ipsum vel recusandae modi. Est autem quam id assumenda nulla aut
+similique sunt hic quia quia vel ducimus quam ut fuga voluptas sit sint velit.
+Aut voluptatem consectetur eum modi omnis est officia animi ea dolor quidem et
+culpa dolor sed suscipit fugit et dignissimos autem. Vel delectus repellat qui
+quis voluptate non praesentium magni qui ipsum culpa eos eaque voluptate.
+
+Qui aspernatur dolorum cum nesciunt recusandae 33 doloremque maiores! Cum quam
+quod et praesentium eius sit exercitationem iste ut voluptas dolor id nobis
+esse aut omnis perspiciatis.
+
+Quo fugiat fugiat non omnis eius rem modi ratione et quos saepe sed illo quae
+et rerum explicabo. Vel quia dolorem ea tenetur beatae quo dolorem galisum nam
+excepturi sint id quasi voluptas. Sit harum quae ut galisum dignissimos sed
+suscipit voluptatum.
+
+"""
--- seccao005/aula155.py
+++ seccao005/aula155.py
@@ -28,8 +28,46 @@
 #     ...
 
 
-Foo = type('Foo', (object,), {})
-f = Foo()
-# print(isinstance(f, Foo))
-print(type(f))
-print(type(Foo))
+# Foo = type('Foo', (object,), {})
+# f = Foo()
+# # print(isinstance(f, Foo))
+# print(type(f))
+# print(type(Foo))
+def meu_repr(self):
+    return f'{type(self).__name__}({self.__dict__})'
+
+
+class Meta(type):
+    def __new__(mcs, name, bases, dct):
+        print('METACLASS NEW')
+        cls = super().__new__(mcs, name, bases, dct)
+        cls.attr = 1234
+        cls.__repr__ = meu_repr
+        if 'falar' not in cls.__dict__ or not callable(cls.__dict__['falar']):
+            raise NotImplementedError('Implemente falar')
+        return cls
+
+    def __call__(self, *args, **kwargs):
+        instancia = super().__call__(*args, **kwargs)
+        if 'nome' not in instancia.__dict__:
+            raise NotImplementedError('Crie attr o nome')
+        return instancia
+
+
+class Pessoa(metaclass=Meta):
+    def __new__(cls, *args, **kwargs):
+        print('MEU NEW')
+        instancia = super().__new__(cls)
+        return instancia
+
+    def __init__(self, nome):
+        print('MEU INIT')
+        # self.nome = nome
+
+    def falar(self):
+        print('Falando...')
+
+
+p1 = Pessoa('Heraldo')
+print(p1.attr)
+p1.falar()
